8e804db45eb7b89fe63c7062c83fdd1cf171905e
Christian Brown <robrowno@icloud.com>
2022-11-09 19:55:03 +0000

    updated allauth templates and profile section

--- profiles/models.py
+++ profiles/models.py
@@ -8,11 +8,10 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    User Profile Model for maintaining default delivery information
-    and order history
+    A user profile model for maintaining default
+    delivery information and order history
     """
-
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -28,9 +27,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update user profile
+    Create or update the user profile
     """
-
     if created:
         UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
     instance.userprofile.save()
