bc3868af41ed8764593be22cceb7ddece842a3d4
Christian Brown <robrowno@icloud.com>
2022-11-09 19:12:28 +0000

    added profiles app and associated files

---
+++ profiles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def profile(request):
+
+    """
+    Display the user's profile
+    """
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
